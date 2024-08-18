f2cc48f22c82018e0947f398a29eb05708b56181
Alex Mullins <alexmullins@hotmail.co.uk>
2024-08-18 19:48:02 +0000

    Add follower app and create model and serializer. Create views FollowerList using generic view.

---
+++ followers/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics, permissions
+from kinnect_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
+
+class FollowerList(generics.ListCreateAPIView):
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
