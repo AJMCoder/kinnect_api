0228c3cd13f519c0a047bdecdd3532965213a08b
Alex Mullins <alexmullins@hotmail.co.uk>
2024-08-19 21:26:10 +0000

    Add a filter field to the comments.views file using djangofilterbackend.

--- comments/views.py
+++ comments/views.py
@@ -2,11 +2,14 @@ from rest_framework import generics, permissions
 from kinnect_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailsSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CommentList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post',]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
