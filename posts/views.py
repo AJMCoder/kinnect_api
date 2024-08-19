ba1412a5bd64a60c8e0bca98a85f816c48a99771
Alex Mullins <alexmullins@hotmail.co.uk>
2024-08-19 21:13:24 +0000

    Fixed an issue with the likes.model where the related_name was missing causing issues within the post.views file when trying to add filters. Add filter field feature.

--- posts/views.py
+++ posts/views.py
@@ -3,17 +3,25 @@ from rest_framework import generics, permissions, filters
 from .models import Post
 from .serializers import PostSerializer
 from kinnect_api.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.annotate(
-        likes_count=Count('like', distinct=True),
+        likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)).order_by('-created_at'
     )
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     filter_backends = [
         filters.OrderingFilter,
-        filters.SearchFilter]
+        filters.SearchFilter,
+        DjangoFilterBackend]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+
     search_fields = [
         'owner__username',
         'title',]
@@ -28,7 +36,7 @@ class PostList(generics.ListCreateAPIView):
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.annotate(
-        likes_count=Count('like', distinct=True),
+        likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)).order_by('-created_at'
     )
     serializer_class = PostSerializer
