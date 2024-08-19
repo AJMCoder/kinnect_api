f04162bfb9a4695a5eceb22b6350a4ff8b6e0ad7
Alex Mullins <alexmullins@hotmail.co.uk>
2024-08-19 20:40:20 +0000

    Add a likes and follower/followed field into the profiles and posts.view files.

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,29 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from .models import Post
 from .serializers import PostSerializer
 from kinnect_api.permissions import IsOwnerOrReadOnly
 
 class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('like', distinct=True),
+        comments_count=Count('comment', distinct=True)).order_by('-created_at'
+    )
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('like', distinct=True),
+        comments_count=Count('comment', distinct=True)).order_by('-created_at'
+    )
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
--- profiles/views.py
+++ profiles/views.py
@@ -1,19 +1,30 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from kinnect_api.permissions import IsOwnerOrReadOnly
 
 class ProfileList(generics.ListCreateAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+        posts_count=Count('owner__post', distinct=True)).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [filters.SearchFilter]
+    ordering_fields = [
+        'followers_count',
+        'following_count',
+        'posts_count',
+        'owner__followed__created_at',
+        'owner__following__created_at',]
 
     def get_serializer_context(self):
         return {'request': self.request}
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+        posts_count=Count('owner__post', distinct=True)).order_by('-created_at')
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_serializer_context(self):
-        return {'request': self.request}
