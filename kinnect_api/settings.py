7d99410fcd21f82577c845a2ee67c9e45764ab77
Alex Mullins <alexmullins@hotmail.co.uk>
2024-09-25 09:59:31 +0000

    Updated the REST_AUTH format to a newer version to help fix an autherisation issue on the front end.

--- kinnect_api/settings.py
+++ kinnect_api/settings.py
@@ -45,14 +45,13 @@ if 'DEV' not in os.environ:
 import os
 import re
 
-REST_USE_JWT = True
-JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = 'my-app-auth'
-JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-JWT_AUTH_SAMESITE = 'None'
-
-REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'kinnect_api.serializers.CurrentUserSerializer'
+REST_AUTH = {
+    'USE_JWT': True,
+    'JWT_AUTH_SECURE': True,
+    'JWT_AUTH_COOKIE': 'my-app-auth',
+    'JWT_AUTH_REFRESH_COOKIE': 'my-refresh-token',
+    'JWT_AUTH_SAMESITE': 'None',
+    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer',
 }
 
 # Quick-start development settings - unsuitable for production
